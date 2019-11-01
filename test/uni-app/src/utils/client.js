2a05fe1d041417c0a8aa805d5a44d6276773a2be
Sake <pan.ziqiang@holytiny.com>
2019-11-01 11:58:59 +0800

    fix：使能debu组件的全局log.

--- test/uni-app/src/utils/client.js
+++ test/uni-app/src/utils/client.js
@@ -3,6 +3,13 @@ import auth from '@feathersjs/authentication-client';
 import feathersSocketioClient from '../../../../packages/feathersjs-wxmp.socket.io-client';
 import io from '../../../../packages/wxmp.socket.io-client';
 
+import Debug from 'debug';
+if ('production'.localeCompare(process.env.NODE_ENV) !== 0) {
+  Debug.enable('*');
+}
+const debug = Debug('uni-app-socket.io-client');
+debug('create socketio init');
+
 const socketioInit = io('http://localhost:3030', {
   transports: ['websocket'],
   forceNew: true,
