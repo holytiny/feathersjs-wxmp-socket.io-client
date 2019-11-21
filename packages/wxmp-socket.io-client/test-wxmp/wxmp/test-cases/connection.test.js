33c6d2f6a10f2edb497aa0d3fa3c6b28109377f2
Sake <pan.ziqiang@holytiny.com>
2019-11-21 17:04:25 +0800

    add: connection tests

--- packages/wxmp-socket.io-client/test-wxmp/wxmp/test-cases/connection.test.js
+++ packages/wxmp-socket.io-client/test-wxmp/wxmp/test-cases/connection.test.js
@@ -48,12 +48,33 @@ function shouldStartTwoConnectionsWithSamePath () {
     } else {
       resolve(false);
     }
-  })
+  });
 }
 shouldStartTwoConnectionsWithSamePath.description = 'should start two connections with same path';
+
+function shouldStartTwoConnectionsWithSamePathAndDifferentQuerystrings () {
+  const io = require('@holytiny/wxmp-socket.io-client/socket.io.dev');
+  return new Promise((resolve, reject) => {
+    let s1 = io('http://localhost:3210/?woot', {
+      transports: ['websocket']
+    });
+    let s2 = io('http://localhost:3210', {
+      transports: ['websocket']
+    });
+    console.log('s1: ', s1, 's2: ', s2);
+    if (s1 !== s2) {
+      resolve(true);
+    } else {
+      resolve(false);
+    }
+  });
+}
+shouldStartTwoConnectionsWithSamePathAndDifferentQuerystrings.description = 'should start two connections with same path and different querystrings';
+
 
 module.exports = [
   shouldConnectToLocalhost,
   shouldNotConnectWhenAutoConnectOptionSetToFalse,
-  shouldStartTwoConnectionsWithSamePath
+  shouldStartTwoConnectionsWithSamePath,
+  shouldStartTwoConnectionsWithSamePathAndDifferentQuerystrings
 ];
